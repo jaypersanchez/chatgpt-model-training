0900135af75f86c0713ef619c90b257118632344
Jayper <jaypersanchez@gmail.com>
2023-04-16 12:44:50 +0800

    feeding data from csv before embedding.  need higher max tokens

--- semantic_search.py
+++ semantic_search.py
@@ -4,27 +4,47 @@ import openai
 import os
 # Import psycopg2 for database connectivity - Postgresql
 import psycopg2
+import json
+import csv
 
 # embedding model parameters
 embedding_model = "text-embedding-ada-002"
 embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
 max_tokens = 8000  # the maximum for text-embedding-ada-002 is 8191
+# Token Tuning 
+token_tuning_params = {
+    "num_tokens": 4, # the number of tokens to use
+    "max_tokens": 10, # the maximum number of tokens to generate
+    "min_token_length": 4, # the minimum length of each token
+    "max_token_length": 8 # the maximum length of each token
+}
 
 # Initialize the OpenAI class
-openai.organization = "org-PwafGfC5oVQgjaAzYFmgp1ep"
-openai.api_key = "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"
+#openai.organization = "org-PwafGfC5oVQgjaAzYFmgp1ep"
+#openai.api_key = "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"
+openai.organization = os.environ['OPENAI_ORG_KEY']
+openai.api_key = os.environ['OPENAI_API_KEY']
+
 #check if we are authenticated
 modelList = openai.Engine.list()
 #print out the available data model for embedding
 #print(modelList)
 
-res = openai.Embedding.create(
-    input = [
-        "Sample document text goes here",
-        "there will be several phrases in each batch"
-    ], engine = embedding_model
-)
-#print(res)
+# load the csv file 
+csv_data = [] 
+with open('./models/exchange.csv', 'r', encoding='utf-8') as csvfile: 
+    reader = csv.reader(csvfile) 
+    #for row in reader: 
+        #csv_data.append(row) 
+    for i, row in enumerate(reader):
+        if i < 100:
+            csv_data.append(reader)
+    i += 1
+            
+# convert the csv data into a json format 
+json_data = json.dumps(csv_data)
+res = openai.Embedding.create(data=json_data, model=embedding_model,input=json_data, params=max_tokens)
+print(res)
 embeddeing_list = [item["embedding"] for item in res["data"]]
 print(embeddeing_list)
 
